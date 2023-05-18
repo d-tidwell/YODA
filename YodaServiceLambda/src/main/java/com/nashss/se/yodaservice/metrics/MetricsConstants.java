96bc688e452b8e345b0d1f914f4c74d6f76034f1
dtidwell <conversations.and.L@gmail.com>
2023-05-18 11:54:19 -0500

    boiler plate setup

---
+++ YodaServiceLambda/src/main/java/com/nashss/se/yodaservice/metrics/MetricsConstants.java
@@ -0,0 +1,12 @@
+package com.nashss.se.yodaservice.metrics;
+
+/**
+ * Constant values for use with metrics.
+ */
+public class MetricsConstants {
+//    public static final String GETPLAYLIST_PLAYLISTNOTFOUND_COUNT = "GetPlaylist.PlaylistNotFoundException.Count";
+
+    public static final String SERVICE = "Service";
+    public static final String SERVICE_NAME = "YodaService";
+    public static final String NAMESPACE_NAME = "CAP/YodaService";
+}
