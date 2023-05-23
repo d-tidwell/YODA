c185e408f7bcdda386ac97310f2112ae5eb2b29a
dtidwell <conversations.and.L@gmail.com>
2023-05-23 14:38:07 -0500

    landing page

---
+++ web/src/pages/landingPage.js
@@ -0,0 +1,45 @@
+import yodaClient from '../api/dannaClient';
+import BindingClass from "../util/bindingClass";
+import Header from '../components/dannaHeader';
+import DataStore from "../util/DataStore";
+
+class LandingPage extends BindingClass {
+    constructor() {
+        super();
+        this.bindClassMethods(['clientLoaded', 'mount', 'login'], this);
+        this.dataStore = new DataStore();
+        this.header = new Header(this.dataStore);
+        // console.log("viewprofile constructor");
+        this.client = new dannaClient();
+        this.clientLoaded();
+    }
+
+    async clientLoaded() {}
+
+    async mount() {
+        const loggedIn = await this.client.isLoggedIn();
+        if(loggedIn){
+            window.location.href= "/desktop.html";
+        }
+        document.getElementById('logout').addEventListener('click', this.login);
+        document.getElementById('sign-up').addEventListener('click', this.login);
+
+    }
+
+    async login(){
+        await this.client.login();
+
+    }
+
+
+
+}
+/**
+ * Main method to run when the page contents have loaded.
+ */
+const main = async () => {
+    const landingPage = new LandingPage();
+    landingPage.mount();
+};
+
+window.addEventListener('DOMContentLoaded', main);
